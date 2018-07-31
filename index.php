83f8144e5d40f7ae98245adcc561beafb3de4143
EullerP <eullerpatrick2@gmail.com>
2018-07-31 20:58:40 -0300

    Add form login

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="em">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width-device-width initial-select=1"/>
+		<meta name="robots" content="follow" >
+
+		<title>Page title</title>
+
+		<base href="/"/>
+		<link rel="styleskeet" href="https://cdnjs.cloudfare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
+	</head>
+
+	<body> 
+		
+			<div "uk-section uk-container uk-test-contqiner">	
+			<div class="uk-grid uk-gridchild-width-1-3@s uk-child-width-1-" uk-grid>
+		<form class="uk-form-stacked js-login">
+
+		    <div class="uk-margin">
+			 <label class="uk-form-label" for="form-stacked-text">Email</label>
+			 <div class="uk-form-controls">
+			     <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
+			 </div>
+		    </div>
+
+		    <div class="uk-margin">
+			 <label class="uk-form-label" for="form-stacked-text">Password</label>
+			 <div class="uk-form-controls">
+			     <input class="uk-input" id="form-stacked-text" type="password" required='required'  placeholder="Your password">
+			 </div>
+		    </div>
+
+		    <div class="uk-margin">
+			 <button class="uk-button uk-button-default" type="submit">LOGON</button>
+		    </div>
+
+			</div>
+		</div>
+
+		<!--jQuery is required-->
+		<script src="https://cdnjs.cloudfare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+		<!--UIkit JS-->
+		<script src="https://cdnjs.cloudfare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
+		<script src="https://cdnjs.cloudfare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.icons.min.js0"></script>
+	</body>
+</html>
