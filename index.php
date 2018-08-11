f9229dc5f50eb5017205d41f7526afc074feb490
EullerP <eullerpatrick2@gmail.com>
2018-08-11 17:09:08 -0300

    Added config: added login/reg pages: added footer

--- index.php
+++ index.php
@@ -1,49 +1,40 @@
+<?php 
+
+	// Allow the config
+	define('__CONFIG__', true);
+	// Require the config
+	require_once "inc/config.php"; 
+
+?>
+
 <!DOCTYPE html>
-<html lang="em">
-	<head>
-		<meta charset="utf-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-		<meta name="viewport" content="width-device-width initial-select=1"/>
-		<meta name="robots" content="follow" >
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="follow">
 
-		<title>Page title</title>
+    <title>Page Title</title>
 
-		<base href="/"/>
-		<link rel="styleskeet" href="https://cdnjs.cloudfare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
-	</head>
+    <base href="/" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
+  </head>
 
-	<body> 
-		
-			<div "uk-section uk-container uk-test-contqiner">	
-			<div class="uk-grid uk-gridchild-width-1-3@s uk-child-width-1-" uk-grid>
-		<form class="uk-form-stacked js-login">
+  <body>
 
-		    <div class="uk-margin">
-			 <label class="uk-form-label" for="form-stacked-text">Email</label>
-			 <div class="uk-form-controls">
-			     <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
-			 </div>
-		    </div>
-
-		    <div class="uk-margin">
-			 <label class="uk-form-label" for="form-stacked-text">Password</label>
-			 <div class="uk-form-controls">
-			     <input class="uk-input" id="form-stacked-text" type="password" required='required'  placeholder="Your password">
-			 </div>
-		    </div>
-
-		    <div class="uk-margin">
-			 <button class="uk-button uk-button-default" type="submit">LOGON</button>
-		    </div>
-
-			</div>
+  	<div class="uk-section uk-container">
+  		<?php
+			echo "Hello Worlld. Today is: ";
+			echo date("Y m d");
+		?>		
+		<p>
+			<a href="/login.php">Login</a>
+			<a href="/register.php">Register</a>
+		</p>
 		</div>
 
-		<!--jQuery is required-->
-		<script src="https://cdnjs.cloudfare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-		<!--UIkit JS-->
-		<script src="https://cdnjs.cloudfare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
-		<script src="https://cdnjs.cloudfare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.icons.min.js0"></script>
-	</body>
+		
+  	<?php require_once "inc/footer.php"; ?> 
+  </body>
 </html>
